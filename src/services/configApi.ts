3b6028b1b5408a9323c1ceb945aa83569e4337c0
Le Chi Dai <lechidai@pixelcent.com>
2022-03-24 14:36:59 +0700

    update env

--- src/services/configApi.ts
+++ src/services/configApi.ts
@@ -1,15 +1,14 @@
 import { EKey } from './../models/general';
 import axios from 'axios';
 
-const lang = localStorage.getItem('lang');
-const token = localStorage.getItem(EKey.TOKEN);
-
 const api = axios.create({
   baseURL: `${process.env.REACT_APP_BASE_API_URL}`,
 });
 
 api.interceptors.request.use(
   (config) => {
+    const lang = localStorage.getItem('lang');
+    const token = localStorage.getItem(EKey.TOKEN);
     if (token) config.headers.common['Authorization'] = `Bearer ${token}`;
     if (lang) config.headers.common['lang'] = lang;
     return config;
