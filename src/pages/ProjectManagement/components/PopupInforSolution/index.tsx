e77bcc86c264f92ec0b6089878fc9137b4a120de
Le Chi Dai <lechidai@pixelcent.com>
2022-04-20 18:08:55 +0700

    fix bug

--- src/pages/ProjectManagement/components/PopupInforSolution/index.tsx
+++ src/pages/ProjectManagement/components/PopupInforSolution/index.tsx
@@ -1,5 +1,5 @@
 import { memo } from 'react';
-import { Dialog, Grid, IconButton } from '@mui/material';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Grid, IconButton } from '@mui/material';
 import classes from './styles.module.scss';
 
 import Buttons from 'components/Buttons';
@@ -23,18 +23,18 @@ const PopupInforSolution = memo((props: PopupPopupInforSolution) => {
       onClose={onCancel}
       classes={{ paper: classes.paper }}
     >
-      <Grid className={classes.root}>
-        <Grid className={classes.header}>
-          <p className={classes.title}>{solution?.title}</p>
-          <IconButton onClick={onCancel}>
-            <img src={Images.icClose} alt='' />
-          </IconButton>
-        </Grid>
-        <Grid className={classes.body}>
-          <div className='ql-editor' dangerouslySetInnerHTML={{__html: solution?.content || ''}}></div>
-        </Grid>
-        <Buttons children="Get started" btnType='Blue' padding='10px 16px' width='91%' onClick={onSelect} className={classes.btn}/>
-      </Grid>
+      <DialogTitle className={classes.header}>
+        <p className={classes.title}>{solution?.title}</p>
+        <IconButton onClick={onCancel}>
+          <img src={Images.icClose} alt='' />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent className={classes.body} dividers>
+        <div className='ql-editor' dangerouslySetInnerHTML={{ __html: solution?.content || '' }}></div>
+      </DialogContent>
+      <DialogActions className={classes.btnBox}>
+        <Buttons children="Get started" btnType='Blue' padding='10px 16px' width='100%' onClick={onSelect}/>
+      </DialogActions>
     </Dialog>
   );
 });
