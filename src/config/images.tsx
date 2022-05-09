4ee508730656264d145a66d3e77257c82f259dd9
tltson299 <truongson1128@gmail.com>
2022-05-09 13:52:13 +0700

    [FE - Setup survey] Update icons

--- src/config/images.tsx
+++ src/config/images.tsx
@@ -55,7 +55,6 @@ import icArrowRegister from 'assets/img/icon/ic-arrow-register.svg';
 import icView from 'assets/img/icon/ic-view.svg';
 import icInvoice from 'assets/img/icon/ic-invoice.svg';
 import imgPaymentFailed from 'assets/img/img-payment-failed.svg';
-import icEditQuestion from 'assets/img/icon/ic-edit-question.svg';
 import icDrag from 'assets/img/icon/ic-drag.svg';
 import icOpenQuestion from 'assets/img/icon/ic-open-question.svg';
 import icSingleChoice from 'assets/img/icon/ic-single-choice.svg';
@@ -120,7 +119,6 @@ const images = {
   icArrowLogin, 
   icArrowRegister,
   imgPaymentFailed,
-  icEditQuestion,
   icDrag,
   icOpenQuestion,
   icSingleChoice,
