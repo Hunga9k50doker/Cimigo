ed0f14e87f75164dc30e959893e7bed6f2ca857e
Congxu <anhcong1843@gmail.com>
2022-09-07 11:26:58 +0700

    22th Login update

--- src/pages/Authentication/Login/index.tsx
+++ src/pages/Authentication/Login/index.tsx
@@ -97,7 +97,6 @@ const Login = () => {
         <Grid className={classes.body}>
           <Heading2 className={classes.textLogin} translation-key="login_title">{t('login_title')}</Heading2>
           <ParagraphSmall className={classes.textHead} translation-key="login_text_login">{t('login_text_login')}</ParagraphSmall>
-          {/* ParagraphSmall: translation-key={null} */}
           <Stack spacing={2} className={classes.textInput}>
             <InputTextfield
               title={t('field_email_address')}
