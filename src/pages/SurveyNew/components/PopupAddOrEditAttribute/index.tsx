34ed9ea9620c80413e7ad4896705f9fbd4992cf6
tienphampixelcent <tienpham@pixelcent.com>
2023-01-13 18:16:10 +0700

    second update trans for popups

--- src/pages/SurveyNew/components/PopupAddOrEditAttribute/index.tsx
+++ src/pages/SurveyNew/components/PopupAddOrEditAttribute/index.tsx
@@ -18,6 +18,7 @@ import Button, {BtnType} from "components/common/buttons/Button";
 import ParagraphSmall from 'components/common/text/ParagraphSmall';
 import ParagraphBody from 'components/common/text/ParagraphBody';
 import Tip from 'components/icons/IconTip';
+import ProjectHelper from 'helpers/project';
 
 
 export interface UserAttributeFormData {
@@ -79,6 +80,8 @@ const PopupAddOrEditAttribute = memo((props: Props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [itemEdit])
 
+  const prefix_trans = useMemo(() => ProjectHelper.getPrefixTrans(project?.solution?.typeId), [project?.solution?.typeId])
+
   return (
     <Dialog
       scroll="paper"
@@ -89,15 +92,15 @@ const PopupAddOrEditAttribute = memo((props: Props) => {
       <form autoComplete="off" className={classes.form} noValidate onSubmit={handleSubmit(_onSubmit)}>
         <DialogTitle $backgroundColor="--white" className={classes.header}>
           {itemEdit ? (
-            <Heading3 $colorName="--gray-90" translation-key="solution_type_1_setup_survey_popup_edit_your_own_att_title">{t('solution_type_1_setup_survey_popup_edit_your_own_att_title')}</Heading3>
+            <Heading3 $colorName="--gray-90" translation-key={`${prefix_trans}_setup_survey_popup_edit_your_own_att_title`}>{t(`${prefix_trans}_setup_survey_popup_edit_your_own_att_title`)}</Heading3>
           ) : (
-            <Heading3 $colorName="--gray-90" translation-key="solution_type_1_setup_survey_popup_add_your_own_att_title">{t('solution_type_1_setup_survey_popup_add_your_own_att_title')}</Heading3>
+            <Heading3 $colorName="--gray-90" translation-key={`${prefix_trans}_setup_survey_popup_add_your_own_att_title`}>{t(`${prefix_trans}_setup_survey_popup_add_your_own_att_title`)}</Heading3>
           )}
           <ButtonClose $backgroundColor="--eerie-black-5" $colorName="--eerie-black-40" onClick={onCancel}>
           </ButtonClose>
         </DialogTitle>
         <DialogContent className={classes.body} dividers>
-          <ParagraphSmall $colorName="--eerie-black" translation-key="solution_type_1_setup_survey_popup_your_own_att_sub_title">{t('solution_type_1_setup_survey_popup_your_own_att_sub_title')}</ParagraphSmall>
+          <ParagraphSmall $colorName="--eerie-black" translation-key={`${prefix_trans}_setup_survey_popup_your_own_att_sub_title`}>{t(`${prefix_trans}_setup_survey_popup_your_own_att_sub_title`)}</ParagraphSmall>
           <Grid container classes={{ root: classes.rootList }}>
             <ListItem
               alignItems="center"
@@ -108,19 +111,19 @@ const PopupAddOrEditAttribute = memo((props: Props) => {
               <ListItemText>
                 <Grid>
                   <Grid item>
-                    <ParagraphBody $fontWeight="600" $colorName="--eerie-black" translation-key="solution_type_1_setup_survey_popup_your_own_att_question">{t('solution_type_1_setup_survey_popup_your_own_att_question')}</ParagraphBody>
-                    <ParagraphSmall $colorName="--eerie-black" className={classes.subInputTitle} translation-key="solution_type_1_setup_survey_popup_your_own_att_input_label">{t('solution_type_1_setup_survey_popup_your_own_att_input_label')}</ParagraphSmall>
+                    <ParagraphBody $fontWeight="600" $colorName="--eerie-black" translation-key={`${prefix_trans}_setup_survey_popup_your_own_att_question`}>{t(`${prefix_trans}_setup_survey_popup_your_own_att_question`)}</ParagraphBody>
+                    <ParagraphSmall $colorName="--eerie-black" className={classes.subInputTitle} translation-key={`${prefix_trans}_setup_survey_popup_your_own_att_input_label`}>{t(`${prefix_trans}_setup_survey_popup_your_own_att_input_label`)}</ParagraphSmall>
                     <InputTextField
                       className={classes.inputPoint}
                       name="content"
-                      placeholder={t('solution_type_1_setup_survey_popup_your_own_att_input_placeholder')}
-                      translation-key-placeholder="solution_type_1_setup_survey_popup_your_own_att_input_placeholder"
+                      placeholder={t(`${prefix_trans}_setup_survey_popup_your_own_att_input_placeholder`)}
+                      translation-key-placeholder={`${prefix_trans}_setup_survey_popup_your_own_att_input_placeholder`}
                       inputRef={register('content')}
                       startAdornment={
                         <InputAdornment position="start">
                           <Tooltip
-                            translation-key="solution_type_1_setup_survey_popup_your_own_att_question_tooltip_icon"
-                            title={t("solution_type_1_setup_survey_popup_your_own_att_question_tooltip_icon")}
+                            translation-key={`${prefix_trans}_setup_survey_popup_your_own_att_question_tooltip_icon`}
+                            title={t(`${prefix_trans}_setup_survey_popup_your_own_att_question_tooltip_icon`)}
                           >
                             <div className={classes.iconLanguage}>{project?.surveyLanguage}</div>
                           </Tooltip>
@@ -144,14 +147,14 @@ const PopupAddOrEditAttribute = memo((props: Props) => {
                   marginLeft: expanded ? "0px" : "12px"
                 }}
                 $fontWeight="700"
-                translation-key="solution_type_1_setup_survey_popup_your_own_att_tip_title"
+                translation-key={`${prefix_trans}_setup_survey_popup_your_own_att_tip_title`}
               >
-                {t('solution_type_1_setup_survey_popup_your_own_att_tip_title')}:
+                {t(`${prefix_trans}_setup_survey_popup_your_own_att_tip_title`)}:
               </Heading6>
               <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <ParagraphSmall className={classes.itemTip}><span className={classes.itemDotTip} translation-key="solution_type_1_setup_survey_popup_your_own_att_tip_description_1"></span>{t('solution_type_1_setup_survey_popup_your_own_att_tip_description_1')}</ParagraphSmall>
-                <ParagraphSmall className={classes.itemTip}><span className={classes.itemDotTip} translation-key="solution_type_1_setup_survey_popup_your_own_att_tip_description_2"></span>{t('solution_type_1_setup_survey_popup_your_own_att_tip_description_2')}</ParagraphSmall>
-                <ParagraphSmall className={classes.itemTip}><span className={classes.itemDotTip} translation-key="solution_type_1_setup_survey_popup_your_own_att_tip_description_3"></span>{t('solution_type_1_setup_survey_popup_your_own_att_tip_description_3')}</ParagraphSmall>
+                <ParagraphSmall className={classes.itemTip}><span className={classes.itemDotTip} translation-key={`${prefix_trans}_setup_survey_popup_your_own_att_tip_description_1`}></span>{t(`${prefix_trans}_setup_survey_popup_your_own_att_tip_description_1`)}</ParagraphSmall>
+                <ParagraphSmall className={classes.itemTip}><span className={classes.itemDotTip} translation-key={`${prefix_trans}_setup_survey_popup_your_own_att_tip_description_2`}></span>{t(`${prefix_trans}_setup_survey_popup_your_own_att_tip_description_2`)}</ParagraphSmall>
+                <ParagraphSmall className={classes.itemTip}><span className={classes.itemDotTip} translation-key={`${prefix_trans}_setup_survey_popup_your_own_att_tip_description_3`}></span>{t(`${prefix_trans}_setup_survey_popup_your_own_att_tip_description_3`)}</ParagraphSmall>
               </Collapse>
             </Grid>
           </Grid>
